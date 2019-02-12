dd2d9888ab69023969ea5b555be660093ec5b25b
= <=>
2019-02-12 11:14:15 -0300

    testing a function to create a new object

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 var Parse = require('parse/node');
+var express = require('express');
+var app = express();
 
+
+Parse.Cloud.define("singup", async (request) => {
+  // Create a new instance of the user class
+    var user = new Parse.User();
+    user.set("username", "lucas lima");
+    user.set("password", "123123123");
+    user.set("email", "lucas.ala1999@gmail.com");
+
+    user.save(null, {useMasterKey:true})
+    .then((gameScore) => {
+      // Execute any logic that should take place after the object is saved.
+      response.success(object.user);
+    }, (error) => {
+      // Execute any logic that should take place if the save fails.
+      // error is a Parse.Error with an error code and message.
+       response.error(error.message);
+
+    });
+});
 
 Parse.Cloud.define("create", async (request) => {
   const GameScore = Parse.Object.extend("GameScore");
@@ -17,7 +38,11 @@ Parse.Cloud.define("create", async (request) => {
   }, (error) => {
     // Execute any logic that should take place if the save fails.
     // error is a Parse.Error with an error code and message.
-    alert('Failed to create new object, with error code: ' + error.message);
+     response.error(error.message);
 
   });
+});
+
+Parse.Cloud.define("cep", async (request) => {
+  response.success("hello world")
 });
